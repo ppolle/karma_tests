21ce337a4644b7f2bfc8aeed936df79cdc192da9
peter polle <peter.m.polle@gmail.com>
2015-09-26 10:41:44 +0300

    new commit

---
+++ app/test.js
@@ -0,0 +1,15 @@
+/*
+describe('boardController',function(){
+
+describe('board members view', function() {
+
+    beforeEach(function() {
+      browser.get('index.html');
+    });
+	  var boardList = element.all(by.repeater('x in names'));
+	    expect(boardList.count()).toBe(15);
+
+
+
+
+});*/
